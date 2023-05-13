ee9218888c44a85fdcbd15e2d594e26f9a290b54
Michał <64927330+CenturionTheMan@users.noreply.github.com>
2023-05-13 20:09:08 +0200

    Working version

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ def bin_to_tens(bits_arr):
     return res
 
 
+def extend_bin_with_zeros(bits_arr, zeros_amount):
+    zeros_to_add = []
+    for i in range(zeros_amount):
+        zeros_to_add.append(0)
+    zeros_to_add.extend(bits_arr)
+    return zeros_to_add
+
+
 def calculate_s(bits_arr, p, r, k):
     s = 0
     vec_index = 0
@@ -36,8 +44,10 @@ def calculate_s(bits_arr, p, r, k):
 def algorithm(x, p):
     X = tens_to_bin(x)
     P = tens_to_bin(p)
-    r = int(np.ceil(np.log2(p) - 1))
+    r = int(np.ceil(np.log2(p)))
     k = int(np.ceil(len(X) / r))
+    if len(X) < k * r:
+        X = extend_bin_with_zeros(X, k * r - len(X))
     print('x=', x, '| p=', p, '| r=', r, '| k=', k)
     s1 = calculate_s(X, p, r, k)
     s_temp = s1
@@ -45,6 +55,8 @@ def algorithm(x, p):
         S_temp = tens_to_bin(s_temp)
         n_temp = len(S_temp)
         k_temp = int(np.ceil(n_temp/r))
+        if len(S_temp) < k_temp * r:
+            S_temp = extend_bin_with_zeros(S_temp, k_temp * r - len(S_temp))
         s_temp = calculate_s(bits_arr=S_temp, p=p, r=r, k=k_temp)
     result = 0
     if p <= s_temp:
