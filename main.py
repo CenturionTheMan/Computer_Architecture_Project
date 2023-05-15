8765fab2cefa24ce54450b7ab32c3033a8b92921
CenturionTheMan <dziedziakmichal0@gmail.com>
2023-05-15 10:05:27 +0200

    big numbers fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import random
 import numpy as np
 
@@ -35,15 +36,15 @@ def calculate_s(bits_arr, p, r, k):
     for i in range(1,k+1):
         sub_X = bits_arr.pop()
         sub_x = bin_to_tens(sub_X)
-        cons = int(np.power(2, r * (i - 1))) % p
+        cons = int(pow(2, r * (i - 1))) % p
         s = s + sub_x * cons
     return s
 
 
 def modulo_computation_algorithm(x, p):
     X = tens_to_bin(x)
-    r = int(np.ceil(np.log2(p)))
-    k = int(np.ceil(len(X) / r))
+    r = int(math.ceil(math.log2(p)))
+    k = int(math.ceil(len(X) / r))
 
     if len(X) < k * r:
         X = extend_bin_with_zeros(X, k * r - len(X))
@@ -52,26 +53,51 @@ def modulo_computation_algorithm(x, p):
     s1 = calculate_s(X, p, r, k)
 
     s_temp = s1
+    loop_counter = 0
     while s_temp >= 2 * p:
+        loop_counter = loop_counter + 1
         S_temp = tens_to_bin(s_temp)
         n_temp = len(S_temp)
-        k_temp = int(np.ceil(n_temp/r))
+        k_temp = int(math.ceil(n_temp/r))
         if len(S_temp) < k_temp * r:
             S_temp = extend_bin_with_zeros(S_temp, k_temp * r - len(S_temp))
         S_temp = split_bit_array_into_subvectors(S_temp, k_temp)
         s_temp = calculate_s(bits_arr=S_temp, p=p, r=r, k=k_temp)
 
     if p <= s_temp:
-        return s_temp - p
+        return s_temp - p, loop_counter
     else:
-        return s_temp
+        return s_temp, loop_counter
+
+
+def save_in_file(x, p, counter):
+    f = open(f'mod_{p}.csv', "a")
+    f.write(f'{x};{p};{counter}\n')
+    f.close()
+
+
+def save_in_file_with_name(x, p, counter, file_name):
+    f = open(file_name, "a")
+    f.write(f'{x};{p};{counter}\n')
+    f.close()
 
 
 if __name__ == '__main__':
     maxX = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
     maxP = [1,1,1,1,1,1,1,1]
-    x = bin_to_tens(maxX)
-    p = bin_to_tens(maxP)
-    x_mod_p = modulo_computation_algorithm(x, p)
-    print(f'{x}(mod{p}) = {x % p}')
-    print('algorithm result =', x_mod_p)
+    X = bin_to_tens(maxX)
+    P = bin_to_tens(maxP)
+    print("BEGINS...")
+    max_iterations = -1
+    for p in range(3, P, 1):
+        print(f'p = {p}')
+        for x in range(1000000, X, 100000000):
+            x_mod_p, counter = modulo_computation_algorithm(x, p)
+            save_in_file(x, p, counter)
+            if counter > max_iterations:
+                max_iterations = counter
+                save_in_file_with_name(x,p,counter,"current_max.csv")
+
+
+    #print(f'{x}(mod{p}) = {x % p}')
+    #print('algorithm result =', x_mod_p)
