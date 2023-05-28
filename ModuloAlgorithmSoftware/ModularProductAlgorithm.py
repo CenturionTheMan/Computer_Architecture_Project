a046e34cfb742148d5c9a71d47392b72ddbd5016
Michał <64927330+CenturionTheMan@users.noreply.github.com>
2023-05-28 18:32:33 +0200

    Add files via upload
    Modular multiplication update (custom bits per vector calculations)

--- ModuloAlgorithmSoftware/ModularProductAlgorithm.py
+++ ModuloAlgorithmSoftware/ModularProductAlgorithm.py
@@ -1,18 +1,31 @@
 from NumbersManipulations import *
 from ModuloAlgorithm import modulo_computation_algorithm as modulo
 
+
 def calculate_modular_product(a: int, b: int, p: int):
-    operands_len = 6
-    subvectors_amount = 2
-    bits_per_subvector = operands_len/2
     a_bin = tens_to_bin(a)
     b_bin = tens_to_bin(b)
 
-    if len(a_bin) < operands_len:
-        a_bin = extend_bin_with_zeros(a_bin, operands_len - len(a_bin))
+    if len(a_bin) < len(b_bin):
+        a_bin = extend_bin_with_zeros(a_bin, len(b_bin) - len(a_bin))
+    elif len(b_bin) < len(a_bin):
+        b_bin = extend_bin_with_zeros(b_bin, len(a_bin) - len(b_bin))
 
-    if len(b_bin) < operands_len:
-        b_bin = extend_bin_with_zeros(b_bin, operands_len - len(b_bin))
+    bits_per_subvector = -1
+    subvectors_amount = -1
+    while bits_per_subvector < 0:
+        if len(a_bin) % 4 == 0:
+            bits_per_subvector = 4
+            subvectors_amount = len(a_bin)/bits_per_subvector
+        elif len(a_bin) % 3 == 0:
+            bits_per_subvector = 3
+            subvectors_amount = len(a_bin)/bits_per_subvector
+        elif len(a_bin) % 2 == 0:
+            bits_per_subvector = 2
+            subvectors_amount = len(a_bin)/bits_per_subvector
+        else:
+            a_bin = extend_bin_with_zeros(a_bin, 1)
+            b_bin = extend_bin_with_zeros(b_bin, 1)
 
     a_subvectors = split_bit_array_into_subvectors(a_bin, subvectors_amount)
     b_subvectors = split_bit_array_into_subvectors(b_bin, subvectors_amount)
